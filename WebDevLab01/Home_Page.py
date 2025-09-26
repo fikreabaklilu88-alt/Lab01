2b0dbeadefe9dc2b9d3fb51601eea436663c74d3
fikreab.aklilu <fikreabaklilu88@gmail.com>
2025-09-25 22:44:26 -0400

    Add files via upload

---
+++ WebDevLab01/Home_Page.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section B")
+st.subheader("Fikreab Aklilu")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to my Streamlit Web Development Lab01 app!
+You can navigate between the pages using the sidebar to the left.
+
+The following pages are:
+
+1. **Portfolio**: A website that showcases my background, education, professional experience, projects, skills, and activites
+
+2. **Quiz**: A fun BuzzFeed-style quiz created to help show you what character you most represent from Avatar: The Last Airbender 
+
+""")
+
+
